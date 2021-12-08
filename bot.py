9979c6bd9dcefdcbc5b53130888295577dcf5a28
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-12-08 19:28:59 +0500

    token change

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
-
 from aiogram import Dispatcher, Bot, types, executor
 import requests
 import json
 from pprint import pprint as print
 btn = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-btn.add("Do`llar -> So`m", "So`m -> Do`llar", "Rubl -> Do`llar", "Do`llar -> Rubl", "Rubl -> So`m", "So`m -> Rubl")
+btn.add("Do`llar >> So`m", "So`m >> Do`llar", "Rubl >> Do`llar", "Do`llar >> Rubl", "Rubl >> So`m", "So`m >> Rubl")
 
-token = '2130953688:AAFmwi8Lp1z2EU-p6uAQVWaosBBkC4qWWdc'
+token = '5052982194:AAFHcIB7lDAZk9PwXWUE34VqFE5knxc8z7k'
 bot = Bot(token=token)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def first(message: types.Message):
-    await bot.send_message(message.chat.id, 'Salom, valyuta kurslarini bilishingiz uchun quyidagi tugmalardan birini bosing.', reply_markup=btn)
+    await bot.send_message(message.chat.id, 'Salom, bu bot orqali siz valyuta kurslarini ossongina bilib olishingiz mumkin. \n Valyuta kurslarini bilishingiz uchun quyidagi tugmalardan birini bosing.', reply_markup=btn)
 
 @dp.message_handler(content_types=['text'])
 async def second(message: types.Message):
@@ -21,7 +20,7 @@ async def second(message: types.Message):
     sum = 'so`m'
     usd = 'do`llor'
     rub = 'rubl'
-    if text == "Do`llar -> So`m":
+    if text == "Do`llar >> So`m":
         inputs = "USD"
         outputs = "UZS"
 
@@ -30,7 +29,7 @@ async def second(message: types.Message):
         result = res['conversion_rates'][outputs]
         await bot.send_message(message.chat.id, f"{result}  {sum}", reply_markup=btn)
 
-    elif text == "So`m -> Do`llar":
+    elif text == "So`m >> Do`llar":
         inputs = "UZS"
         outputs = "USD"
 
@@ -39,7 +38,7 @@ async def second(message: types.Message):
         result = res['conversion_rates'][outputs]
         await bot.send_message(message.chat.id, f"{result}  {usd}", reply_markup=btn)
 
-    elif text == "Rubl -> Do`llar":
+    elif text == "Rubl >> Do`llar":
         inputs = "RUB"
         outputs = "USD"
 
@@ -48,7 +47,7 @@ async def second(message: types.Message):
         result = res['conversion_rates'][outputs]
         await bot.send_message(message.chat.id, f"{result}  {usd}", reply_markup=btn)
 
-    elif text == "Do`llar -> Rubl":
+    elif text == "Do`llar >> Rubl":
         inputs = "USD"
         outputs = "RUB"
 
@@ -57,7 +56,7 @@ async def second(message: types.Message):
         result = res['conversion_rates'][outputs]
         await bot.send_message(message.chat.id, f"{result}  {rub}", reply_markup=btn)
 
-    elif text == "Rubl -> So`m":
+    elif text == "Rubl >> So`m":
         inputs = "RUB"
         outputs = "UZS"
 
@@ -66,7 +65,7 @@ async def second(message: types.Message):
         result = res['conversion_rates'][outputs]
         await bot.send_message(message.chat.id, f"{result}  {sum}", reply_markup=btn)
 
-    elif text == "So`m -> Rubl":
+    elif text == "So`m >> Rubl":
         inputs = "UZS"
         outputs = "RUB"
 
@@ -77,7 +76,7 @@ async def second(message: types.Message):
 
     else:
         await bot.send_message(message.chat.id, '❌ Siz noto`g`ri ma`lumot kiritdingiz, tugmalardan birini tanlashingiz kerak!', reply_markup=btn)
-    
+
 
 
 if __name__ == '__main__':
